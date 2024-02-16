68b58f3abc1bdf96adb4523827104869c1b57682
iamcelestino <trcelestino488@gmail.com>
2024-02-16 01:32:15 -0800

    add click events to addNote__btn, close messages and store title and description in localStorage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import Note from "./logic/logic";
 
 const form = document.querySelector("form");
+const addNoteFormBox = document.querySelector('.addNote__form')
 const contentContainer = document.querySelector(".content__container");
+const errorMessageBox = document.querySelector('.empty__error');
+const closeMessage = document.querySelector('.close__message')
+const addBtn = document.querySelector(".add__note");
 
 const note = new Note(contentContainer);
 
@@ -10,12 +14,25 @@ form.addEventListener("submit", event  => {
     const title = form.title.value;
     const noteDescription = form.noteDescription.value;
     if (noteDescription.length === 0) {
-        console.log("test")
+       errorMessageBox.classList.add('active');
     }
     else {
-        note.addNote(title, noteDescription);
+        note.addNote(title, noteDescription)
         form.reset();
+        addNoteFormBox.classList.remove('active');
     }
+    // note.upateColor(noteColors);
 });
 
 note.deleteNote();
+
+closeMessage.addEventListener('click', () => {
+    errorMessageBox.classList.remove('active');
+});
+
+addBtn.addEventListener('click', () => {
+    addNoteFormBox.classList.add('active');
+});
+
+note.upateColor(document.querySelector(".note__colors"));
+
--- src/logic/logic.js
+++ src/logic/logic.js
@@ -5,24 +5,37 @@ class Note {
         this.contentContainer = container;
     }
     addNote(noteTitle, description) {
+        localStorage.setItem('title', noteTitle)
+        localStorage.setItem ('noteDescription', description);
         const html = `
         <div class="note__container">
-            <h6 class="note_title">${noteTitle}</h6>
-            <p class="note__description">${description}</p>
+            <h6 class="note_title">${localStorage.getItem('title')}</h6>
+            <p class="note__description">${localStorage.getItem('noteDescription')}</p>
             <div class="actions">
                 <ion-icon id="trash_delete" name="trash-outline"></ion-icon>
             </div>
         </div>
         `
-        this.contentContainer.innerHTML += html;
+        this.contentContainer.innerHTML += html;;
     }
+    // upateColor(noteColors) {
+    //     noteColors.addEventListener("click", event => {
+    //         if (event.target.className === "bg-coral") {
+    //            Array.from(this.contentContainer.children).forEach(item => {
+    //                 if (item.className === "note__container") {
+    //                     this.contentContainer.childNodes[3].classList.add('bg-coral');
+    //                 }
+    //            })
+    //         }
+    //     });
+    // }
     deleteNote() {
       this.contentContainer.addEventListener("click", event => {
             const element = event.target.parentElement;
             if (event.target.id === "trash_delete") {
                 element.parentElement.remove();
             }
-      })
+      });
     }
 }
 
